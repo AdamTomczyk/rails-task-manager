fce8416c58c5d9c177d87853eafb6138b99cac9f
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-08-10 15:24:51 +0200

    all done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,49 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+     @task = Task.find(params[:id])
+  end
+
+   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    # Find the restaurant we're editing
+      @task = Task.find(params[:id])
+
+    # Take the form input and update that restaurant
+    @task.update(task_params)
+
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+private
+
+  # STRONG PARAMS
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
